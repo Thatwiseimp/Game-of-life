bbca41d6f32cde4da3852b75cf4f10aca6f3e839
thedoodler <ramprakashsmail@gmail.com>
2020-09-08 18:27:38 +0530

    basic array manipulation

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function makearray(x,y){
+  let array = new Array(x);
+  for (let i = 0; i < x; ++){         
+     }
+}
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() {
+  background(0,0,0);
+  ellipse(100, 200, 50, 50);
+}
